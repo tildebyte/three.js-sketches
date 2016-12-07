75fa91d00fb4f41488f344e628049a84164bdb03
Ben Alkov <balkov1@lenovo.com>
2016-12-07 13:54:54 -0500

    Working

--- BoxClock/BoxClock.js
+++ BoxClock/BoxClock.js
@@ -2,8 +2,7 @@
     A wireframe box with colored edges which expands and contracts according
     to time-of-day.
     An original implementation of *hms* from http://www.gysin-vanetti.com/hms
-    (C) Ben Alkov, 2014, licensed as APL 2.0 as part of processing.py
-    (https://github.com/jdf/processing.py).
+    (C) Ben Alkov, 2016, licensed as.
 */
 
 'use strict'
@@ -13,29 +12,16 @@ const TAU = Math.PI * 2,
     Height = window.innerHeight,
     // (x, a1, a2, b1, b2)
     map = THREE.Math.mapLinear,
-    // (x, y, t)
-    lerp = THREE.Math.lerp,
-    // (value, min, max)
-    clamp = THREE.Math.clamp,
-    // (x, min, max)
-    // Returns the percentage (0-1) that x has moved between min and max,
-    // with easing in to/out of min and max.
-    smoothstep = THREE.Math.smoothstep,
-    degToRad = THREE.Math.degToRad,
-    radToDeg = THREE.Math.radToDeg,
-    // (low, high) Random integer from low to high interval.
-    randInt = THREE.Math.randInt,
-    // (low, high) Random float from low to high interval.
-    randFloat = THREE.Math.randFloat,
-    randFLoatSpread = THREE.Math.randFloatSpread
+    RED = new THREE.Color('rgb(255, 137, 95)'),
+    GREEN = new THREE.Color('rgb(176, 255, 121)'),
+    BLUE = new THREE.Color('rgb(56, 76, 204)'),
+    BACKGROUND = new THREE.Color('rgb(10, 10, 10)')
 
 let scene,
     camera,
     renderer,
-    boxClock,
-    Red = new THREE.Color('rgb(255, 137, 95)'),
-    Green = new THREE.Color('rgb(176, 255, 121)'),
-    Blue = new THREE.Color('rgb(56, 76, 204)')
+    data,
+    box = new THREE.Object3D()
 
 let requestAnimationFrame = window.requestAnimationFrame ||
                             window.mozRequestAnimationFrame ||
@@ -43,55 +29,62 @@ let requestAnimationFrame = window.requestAnimationFrame ||
                             window.msRequestAnimationFrame
 window.requestAnimationFrame = requestAnimationFrame
 
+data = {
+    // Seconds - lines along `x`.
+    'seconds': {
+        'verts': [[-1, 1, 1],
+                  [1, 1, 1],
+                  [-1, -1, 1],
+                  [1, -1, 1],
+                  [-1, -1, -1],
+                  [1, -1, -1],
+                  [-1, 1, -1],
+                  [1, 1, -1]],
+        'color': RED
+    },
+    // Minutes - lines along `y`.
+    'minutes': {
+        'verts': [[-1, 1, 1],
+                  [-1, -1, 1],
+                  [1, 1, 1],
+                  [1, -1, 1],
+                  [1, 1, -1],
+                  [1, -1, -1],
+                  [-1, 1, -1],
+                  [-1, -1, -1]],
+        'color': GREEN
+    },
+    // Hours - lines along `z`.
+    'hours': {
+        'verts': [[-1, 1, -1],
+                  [-1, 1, 1],
+                  [1, 1, -1],
+                  [1, 1, 1],
+                  [1, -1, -1],
+                  [1, -1, 1],
+                  [-1, -1, -1],
+                  [-1, -1, 1]],
+        'color': BLUE
+    }
+}
 
-class Box {
-    constructor() {
-        // Draw a 2x2x2 transparent cube with edges colored according to the
-        // current time.
-        // Seconds - lines along local `x`: Red
-        // Minutes - lines along local `y`: Green
-        // Hours - lines along local `z`: Blue
 
-        this.size = 12
-        this.tick = 0.008
-        this.boxGeometry = new THREE.BoxGeometry(this.size, this.size, this.size)
-        this.wireGeometry = new THREE.EdgesGeometry(this.boxGeometry)
-        this.boxMaterial = new THREE.MeshBasicMaterial({
-            'color': 0x0a0a0a
-            // 'transparent': true,
-            // 'opacity': 0
-        })
-        this.wireMaterial = new THREE.LineBasicMaterial({
-            'color': Blue,
-            // 'transparent': true,
-            // 'opacity': 0.65,
-            'linewidth': 4
-        })
-        this.boxMesh = new THREE.Mesh(this.boxGeometry, this.boxMaterial)
-        this.wireMesh = new THREE.LineSegments(this.wireGeometry,
-                                                  this.wireMaterial)
-        this.boxMesh.add(this.wireMesh)
-        // Offset edges so as to deal with face/edge clashing.
-        this.wireMesh.scale.x += 0.01
-        this.wireMesh.scale.y += 0.01
-        this.wireMesh.scale.z += 0.01
-        this.scale()
+class Edges {
+    constructor(record) {
+        let geometry = new THREE.Geometry(),
+            material = new THREE.LineBasicMaterial()
+            this.record = record
+            this.verts = this.record['verts']
+
+        for (let vert of this.verts) {
+            geometry.vertices.push(new THREE.Vector3().fromArray(vert))
+        }
+        material = new THREE.LineBasicMaterial({'color': this.record['color']})
+        this.linesMesh = new THREE.LineSegments(geometry, material)
     }
 
     getMeshObject() {
-        return this.boxMesh
-    }
-
-    rotate() {
-        this.boxMesh.rotation.x += this.tick
-        this.boxMesh.rotation.y += this.tick
-        this.boxMesh.rotation.z += this.tick
-    }
-
-    scale() {
-        this.boxMesh.scale.x = map(new Date().getSeconds(), 0, 59, 1, 12)
-        this.boxMesh.scale.y = map(new Date().getMinutes(), 0, 59, 1, 12)
-        this.boxMesh.scale.z = map(new Date().getHours(), 0, 23, 1, 12)
+        return this.linesMesh
     }
 }
 
@@ -115,7 +108,7 @@ function webglAvailable() {
 	}
 
 	if (webglAvailable()) {
-		renderer = new THREE.WebGLRenderer()
+		renderer = new THREE.WebGLRenderer({'antialias': true})
 	}
     else {
         let container
@@ -142,24 +135,43 @@ function init() {
         0.1,  // Near clip.
         10000  // Far clip.
     )
-    camera.position.z = 400
+    camera.position.z = 75
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(Width, Height)
-    renderer.setClearColor(0x0a0a0a, 1.0)
+    renderer.setClearColor(BACKGROUND, 1.0)
     document.body.appendChild(renderer.domElement)
     window.addEventListener('resize', onWindowResize, false)
 }
 
 
 function setup() {
-    boxClock = new Box()
-    scene.add(boxClock.getMeshObject())
+    // Draw a 2x2x2 transparent cube with edges colored according to the
+    // current time.
+    // Seconds - lines along local `x`: Red
+    // Minutes - lines along local `y`: Green
+    // Hours - lines along local `z`: Blue
+    box.material = new THREE.MeshBasicMaterial({'color': BLUE})
+    for (let key in data) {
+        if (!data.hasOwnProperty(key)) {continue}
+        let record = data[key]
+        box.add(new Edges(record).getMeshObject())
+    }
+    scene.add(box)
 }
 
 
 function update() {
-    boxClock.rotate()
-    boxClock.scale()
+    let tick = 0.008,
+        s = map(new Date().getSeconds(), 0, 59, 1, 12),
+        m = map(new Date().getMinutes(), 0, 59, 1, 12),
+        h = map(new Date().getHours(), 0, 23, 1, 12)
+
+    box.rotation.x += tick
+    box.rotation.y += tick
+    box.rotation.z += tick
+    box.scale.x = s
+    box.scale.y = m
+    box.scale.z = h
 }
 
 
